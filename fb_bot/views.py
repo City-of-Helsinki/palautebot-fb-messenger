cf69ce0db410dbb5274683b5f376ff0fc783d27b
Viljami Ketola <viljami.ketola@haltu.fi>
2017-07-05 11:53:40 +0300

    testing

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -98,12 +98,9 @@ class FbBotView(generic.View):
         )
         user = new_row.user_id
         default_date = new_row.source_created_at
-        pprint(Feedback.objects.exclude(message='temp'))
-        pprint(Feedback.objects.exclude(message='temp').filter(user_id=user).latest('source_created_at').query)
-        # exclude(message='temp').
-        prev_row = Feedback.objects.filter(user_id=user).latest('source_created_at')
+        prev_row = Feedback.objects.filter(user_id=user).exclude(message='temp').latest('source_created_at')
         pprint('id: %s\nphase: %s\nsource_created_at: %s\nuser_id: %s\nmessage: %s' % (prev_row.id, prev_row.phase, prev_row.source_created_at, prev_row.user_id, prev_row.message))
-        Feedback.objects.filter(id=new_row.id).delete()
+        # Feedback.objects.filter(id=new_row.id).delete()
         return prev_row.phase
 
 
