dfc0c546135e85a43e916b08401fac03f536100f
Viljami Ketola <viljami.ketola@haltu.fi>
2017-07-10 12:10:45 +0300

    fixed variable in string error

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -336,7 +336,7 @@ class FbBotView(generic.View):
                         if row.id == '204695756714834':
                             pprint('Bot cannot post to itself')
                         else:
-                            bot_answer = 'Virheellinen syöte. %s' % (bot_answers[phase])
+                            bot_answer = 'Virheellinen syöte. %s' % (bot_answers[feedback['phase']])
                             post_facebook_message(message['sender']['id'], bot_answer)
         return HttpResponse()
 
