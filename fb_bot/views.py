36952b1a2b51c7fe6201b26c4f43dd84573e4adf
Viljami Ketola <viljami.ketola@haltu.fi>
2017-07-25 16:46:03 +0300

    Added additional check to API response

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -252,6 +252,7 @@ class FbBotView(generic.View):
             data=feedback, headers=headers)
         url_to_feedback = ''
         new_ticket = response_new_ticket.json()
+        print(new_ticket)
         for entry in new_ticket:
             if 'code' in entry:
                 print('ERROR: ', entry['code'])
@@ -265,7 +266,10 @@ class FbBotView(generic.View):
                 return url_to_feedback
         try:
             new_ticket_id = new_ticket[0]['service_request_id']
-            url_to_feedback = 'https://www.hel.fi/helsinki/fi/kaupunki-ja-hallinto/osallistu-ja-vaikuta/palaute/nayta-palaute?fid=%s' % (new_ticket_id)
+            if new_ticket_id == 'failureCode':
+                return url_to_feedback
+            else:
+                url_to_feedback = 'https://www.hel.fi/helsinki/fi/kaupunki-ja-hallinto/osallistu-ja-vaikuta/palaute/nayta-palaute?fid=%s' % (new_ticket_id)
         except KeyError as e:
             print('New data doesn\'t contain service_request_id %s' % (new_ticket))
         return url_to_feedback
