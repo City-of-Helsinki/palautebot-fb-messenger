84041e6beb6152b07d3f40530b9188f58c003803
Viljami Ketola <viljami.ketola@haltu.fi>
2017-07-25 15:19:14 +0300

    Added feedback API connection

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -212,10 +212,6 @@ class FbBotView(generic.View):
         else:
             return prev_row.phase
 
-    def save_to_hki_database(self, feedback):
-        # Send information to HKI database and return url to the feedback
-        return 'www.google.fi'
-
     def init_answers(self):
         # Function initializes answer list
         # Order of items is important because these are being used with
@@ -245,6 +241,35 @@ class FbBotView(generic.View):
             Feedback.objects.filter(id=id).update(phase=phase)
         elif phase == 6:
             Feedback.objects.filter(id=id).update(phase=phase, address='')
+
+
+    def save_to_hki_database(self, feedback):
+        # Send information to HKI database and return url to the feedback
+        feedback['api_key'] = settings.HELSINKI_API_KEY
+        feedback['service_code'] = settings.HELSINKI_API_SERVICE_CODE
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response_new_ticket = requests.post(settings.HELSINKI_POST_API_URL,
+            data=feedback, headers=headers)
+        url_to_feedback = ''
+        new_ticket = response_new_ticket.json()
+        for entry in new_ticket:
+            if 'code' in entry:
+                print('ERROR: ', entry['code'])
+                print('info: ', entry['description'])
+                return url_to_feedback
+            elif 'service_request_id' in entry:
+                break
+            else:
+                print('something wrong with api data')
+                print(entry)
+                return url_to_feedback
+        try:
+            new_ticket_id = new_ticket[0]['service_request_id']
+            url_to_feedback = 'https://www.hel.fi/helsinki/fi/kaupunki-ja-hallinto/osallistu-ja-vaikuta/palaute/nayta-palaute?fid=%s' % (new_ticket_id)
+        except KeyError as e:
+            print('New data doesn\'t contain service_request_id %s' % (new_ticket))
+        return url_to_feedback
+
     def post(self, request, *args, **kwargs):
         # Post function to handle Facebook messages
         feedback = self.init_feedback()
